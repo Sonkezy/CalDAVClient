f166512d65bfe233f2490853b2627b03132a70cb
Andrey Meshkov <sonkezy@mail.ru>
2024-03-07 23:49:50 +0700

    Update CalDAV client

--- src/main.go
+++ src/main.go
@@ -12,40 +12,45 @@ import (
 func resetTerminal() {
 	cmd := exec.Command("reset")
 	cmd.Stdout = os.Stdout
-	cmd.Run()
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Reset termnal error: ", err)
+	}
+	os.Exit(0)
 }
 
 func main() {
-	var caldavUrl string
+	var caldavURL string
 	var clientName string
 	var clientToken string
-	flag.StringVar(&caldavUrl, "url", "https://caldav.yandex.ru", "CalDAV server url")
+	flag.StringVar(&caldavURL, "url", "https://caldav.yandex.ru", "CalDAV server url")
 	flag.StringVar(&clientName, "name", "test", "Server port ")
 	flag.StringVar(&clientToken, "token", "test", "Your id")
 	flag.Parse()
-	client, err := caldav.NewCaldavClient(clientName, clientToken, caldavUrl, context.Background())
+	client, err := caldav.NewCaldavClient(clientName, clientToken, caldavURL)
 	if err != nil {
 		fmt.Println(err)
 	}
-	//client.GetCalendarsNames(context.Background())
+	fmt.Println("Hello")
 	defer resetTerminal()
 	for {
 		var command string
 		fmt.Scan(&command)
 		switch command {
 		case "GetCalendarsNames":
-			calendarNames := client.GetCalendarsNames(context.Background())
+			calendarNames := client.GetCalendarsNames()
 			fmt.Println(calendarNames)
 		case "GetCalendars":
 			resp, err := client.GetCalendars(context.Background(), "Test")
 			if err != nil {
 				fmt.Println(err)
 			} else {
-				fmt.Println(resp)
+				client.OutputEvents(resp)
 			}
+		case "CreateEvent":
+			client.CreateEvent()
 		case "q":
 			resetTerminal()
-			os.Exit(0)
 		default:
 			fmt.Println("Error. Command not recognized")
 		}
--- src/pkg/caldav_client/client.go
+++ src/pkg/caldav_client/client.go
@@ -1,14 +1,17 @@
 package caldavclient
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
-	"github.com/emersion/go-webdav"
-	"github.com/emersion/go-webdav/caldav"
+	"github.com/emersion/go-ical"
+	"github.com/lugamuga/go-webdav"
+	"github.com/lugamuga/go-webdav/caldav"
 	"github.com/pkg/errors"
 )
 
@@ -16,31 +19,40 @@ type CaldavClient struct {
 	caldav.Client
 	Login     string
 	Token     string
-	Url       string
+	URL       string
 	principal string
 	homeset   string
 }
 
-func NewCaldavClient(login, token, url string, ctx context.Context) (*CaldavClient, error) {
+type EventClient struct {
+	Calendar string
+	Name     string
+	Start    time.Time
+	End      time.Time
+	Location string
+	path     string
+}
+
+func NewCaldavClient(login, token, url string) (*CaldavClient, error) {
 	caldavClient, err := caldav.NewClient(webdav.HTTPClientWithBasicAuth(http.DefaultClient, login, token), url)
 	if err != nil {
 		return nil, err
 	}
-	principal, err := caldavClient.FindCurrentUserPrincipal(ctx)
+	principal, err := caldavClient.FindCurrentUserPrincipal()
 	if err != nil {
 		return nil, err
 	}
-	homeset, err := caldavClient.FindCalendarHomeSet(ctx, principal)
+	homeset, err := caldavClient.FindCalendarHomeSet(principal)
 	if err != nil {
 		return nil, err
 	}
-	return &CaldavClient{Client: *caldavClient, Login: login, Token: token, Url: url, principal: principal, homeset: homeset}, nil
+	return &CaldavClient{Client: *caldavClient, Login: login, Token: token, URL: url, principal: principal, homeset: homeset}, nil
 }
 
-func (c *CaldavClient) GetCalendarsNames(ctx context.Context) []string {
-	calendars, err := c.FindCalendars(ctx, c.homeset)
+func (c *CaldavClient) GetCalendarsNames() []string {
+	calendars, err := c.FindCalendars(c.homeset)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return nil
 	}
 	var calendarNames []string
@@ -50,10 +62,10 @@ func (c *CaldavClient) GetCalendarsNames(ctx context.Context) []string {
 	return calendarNames
 }
 
-func (c *CaldavClient) GetCalendarsPaths(ctx context.Context) []string {
-	calendars, err := c.FindCalendars(ctx, c.homeset)
+func (c *CaldavClient) GetCalendarsPaths() []string {
+	calendars, err := c.FindCalendars(c.homeset)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return nil
 	}
 	var calendarPaths []string
@@ -63,29 +75,19 @@ func (c *CaldavClient) GetCalendarsPaths(ctx context.Context) []string {
 	return calendarPaths
 }
 
-func (c *CaldavClient) GetCalendars(ctx context.Context, name string) ([]caldav.CalendarObject /*[]*ical.Component*/, error) {
-	/*calendars, err := c.FindCalendars(ctx, c.homeset)
-	if err != nil {
-		return nil, err
-	}
-		var calendarExist bool = false
-	for _, calendar := range calendars {
-		if calendar.Name == name {
-			calendarExist = true
-		}
-	}
-	if !calendarExist {
-		return nil, errors.New(fmt.Sprintf("Calendar %s not exist", name))
-	}
-	return nil, nil*/
+func (c *CaldavClient) GetCalendars(ctx context.Context, name string) ([]EventClient /*[]caldav.CalendarObject  []*ical.Component*/, error) {
 	events, err := c.loadTodayEvents(ctx)
 	if err != nil {
 		return nil, err
 	}
-	return events, nil
+	eventsList, err := c.ParseEvents(events)
+	if err != nil {
+		return nil, err
+	}
+	return eventsList, nil
 }
 
-func (c *CaldavClient) queryCalendarEventsByTimeRange(ctx context.Context,
+func (c *CaldavClient) queryCalendarEventsByTimeRange(
 	calendarPath string,
 	start time.Time,
 	end time.Time) ([]caldav.CalendarObject, error) {
@@ -101,7 +103,7 @@ func (c *CaldavClient) queryCalendarEventsByTimeRange(ctx context.Context,
 		},
 	}
 
-	return c.QueryCalendar(ctx, calendarPath, query)
+	return c.QueryCalendar(calendarPath, query)
 }
 
 func (c *CaldavClient) GetTodayDateTimes() (time.Time, time.Time) {
@@ -117,23 +119,152 @@ func (c *CaldavClient) GetTodayDateTimes() (time.Time, time.Time) {
 func (c *CaldavClient) loadTodayEvents(ctx context.Context) ([]caldav.CalendarObject, error) {
 	start, end := c.GetTodayDateTimes()
 	var events []caldav.CalendarObject
-	paths := c.GetCalendarsPaths(ctx)
+	paths := c.GetCalendarsPaths()
 	for _, path := range paths {
-		tmp_events, err := c.LoadEvents(ctx, path, start, end)
+		tmpEvents, err := c.LoadEvents(ctx, path, start, end)
 		if err != nil {
 			return nil, err
 		}
-		events = append(events, tmp_events...)
+		events = append(events, tmpEvents...)
 	}
 	return events, nil
 }
 
 func (c *CaldavClient) LoadEvents(ctx context.Context, calendarPath string, start time.Time, end time.Time) ([]caldav.CalendarObject, error) {
-	var events []caldav.CalendarObject
-	calendarObjects, err := c.queryCalendarEventsByTimeRange(ctx, calendarPath, start, end)
+	calendarObjects, err := c.queryCalendarEventsByTimeRange(calendarPath, start, end)
 	if calendarObjects == nil {
 		log.Print("Can't get events for calendar "+calendarPath, " ", c.Login, " ", err)
-		return events, errors.New("Can't get events from calendar")
+		return calendarObjects, errors.New("Can't get events from calendar")
 	}
-	return events, nil
+	return calendarObjects, nil
+}
+
+func (c *CaldavClient) ParseEvents(objects []caldav.CalendarObject) ([]EventClient, error) {
+	var eventsList []EventClient
+	timeLocation, err := time.LoadLocation("Local")
+	if err != nil {
+		log.Println(err)
+	}
+	for _, object := range objects {
+		startTime, err := time.ParseInLocation("20060102T150405Z", (*object.Data).Component.Children[0].Props["DTSTART"][0].Value, timeLocation)
+		if err != nil {
+			return nil, err
+		}
+		endTime, err := time.ParseInLocation("20060102T150405Z", (*object.Data).Component.Children[0].Props["DTEND"][0].Value, timeLocation)
+		if err != nil {
+			return nil, err
+		}
+		var location string
+		if (*object.Data).Component.Children[0].Props["LOCATION"] != nil {
+			location = (*object.Data).Component.Children[0].Props["LOCATION"][0].Value
+		}
+		event := &EventClient{
+			Name:     (*object.Data).Component.Children[0].Props["SUMMARY"][0].Value,
+			Location: location,
+			Start:    startTime,
+			End:      endTime,
+			path:     object.Path,
+		}
+		eventsList = append(eventsList, *event)
+	}
+	return eventsList, nil
+}
+
+func (c *CaldavClient) OutputEvents(eventsList []EventClient) {
+	for n, event := range eventsList {
+		fmt.Printf("%d) Name: %s\n", n+1, event.Name)
+		fmt.Printf("----Calendar: %s\n", event.Calendar)
+		fmt.Printf("----Location: %s\n", event.Location)
+		fmt.Printf("----Start time: %s\n", event.Start.Format(time.UnixDate))
+		fmt.Printf("----End time:   %s\n", event.End.Format(time.UnixDate))
+		fmt.Printf("----Path:   %s\n", event.path)
+	}
+}
+
+func (c *CaldavClient) CreateEvent() {
+	newEvent := &EventClient{}
+	fmt.Println("Type event name: ")
+	fmt.Scan(&newEvent.Name)
+	fmt.Println("Type location name: ")
+	fmt.Scan(&newEvent.Location)
+	fmt.Println("Type event start time (example: 2000-12-31 12:00:00): ")
+	reader := bufio.NewReader(os.Stdin)
+	startTime, _ := reader.ReadString('\n')
+	startTime = startTime[:len(startTime)-1]
+	var err error
+	newEvent.Start, err = time.Parse("2006-01-02 03:04:05", startTime)
+	if err != nil {
+		fmt.Println("Unexpected input ", err)
+		return
+	}
+	fmt.Println("Type event end time (format ): ")
+	endTime, _ := reader.ReadString('\n')
+	if len(endTime) > 19 {
+		endTime = endTime[:19]
+	}
+	newEvent.End, err = time.Parse("2006-01-02 03:04:05", endTime)
+	if err != nil {
+		fmt.Println("Unexpected input ", err)
+		return
+	}
+	if startTime > endTime {
+		fmt.Println("Start time later then end time")
+		return
+	}
+	c.PutEvent(*newEvent)
+}
+
+func (c *CaldavClient) PutEvent(event EventClient) {
+	calendarsPath := c.GetCalendarsPaths()
+	calendarsNames := c.GetCalendarsNames()
+	fmt.Println("Select calendar:")
+	for n, name := range calendarsNames {
+		fmt.Printf("%d) %s\n", n+1, name)
+	}
+	var calendarNumber int
+	fmt.Scan(&calendarNumber)
+	calendarNumber--
+	if calendarNumber < 0 || calendarNumber >= len(calendarsPath) {
+		fmt.Println("Wrong number")
+		return
+	}
+	newEvent := ical.NewCalendar()
+	newEvent.Component.Props.Add(&ical.Prop{
+		Name:  "PRODID",
+		Value: "-//CalDAV //CalDAV Calendar//EN",
+	})
+	newEvent.Component.Props.Add(&ical.Prop{
+		Name:  "VERSION",
+		Value: "2.0",
+	})
+	newComponent := ical.NewComponent("VEVENT")
+	newComponent.Props.Add(&ical.Prop{
+		Name:  "SUMMARY",
+		Value: event.Name,
+	})
+	newComponent.Props.Add(&ical.Prop{
+		Name:  "LOCATION",
+		Value: event.Location,
+	})
+	newComponent.Props.Add(&ical.Prop{
+		Name:  "DTSTART",
+		Value: event.Start.Format("20060102T150405"),
+	})
+	newComponent.Props.Add(&ical.Prop{
+		Name:  "DTEND",
+		Value: event.End.Format("20060102T150405"),
+	})
+	newComponent.Props.Add(&ical.Prop{
+		Name:  "DTSTAMP",
+		Value: event.Start.Format("20060102T150405"),
+	})
+	newComponent.Props.Add(&ical.Prop{
+		Name:  "UID",
+		Value: "testN1",
+	})
+	newEvent.Component.Children = append(newEvent.Component.Children, newComponent)
+	_, err := c.PutCalendarObject(c.homeset, newEvent)
+	if err != nil {
+		log.Println(err)
+	}
 }
